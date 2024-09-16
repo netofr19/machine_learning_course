2d943ef7def2d5eb7fb35a9aacc35a187b12fa96
Francisco Procópio <francisco.procopio@enacom.com.br>
2024-09-16 09:37:07 -0300

    creation of logs

---
+++ python_logging/logs/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+# Configuring logging
+logging.basicConfig(
+    filename='python_logging/logs/app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
